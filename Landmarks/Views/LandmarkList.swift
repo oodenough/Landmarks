80da4c367ae34327b34859cf838368e2146a1b01
Haozilla <haozilla@icloud.com>
2025-03-01 22:05:05 +0800

    refactor: make `LandmarkList` view dynamic

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,18 +9,9 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        LandmarkRow(landmark: landmarks[0])
-        LandmarkRow(landmark: landmarks[1])
-        LandmarkRow(landmark: landmarks[2])
-        LandmarkRow(landmark: landmarks[3])
-        LandmarkRow(landmark: landmarks[4])
-        LandmarkRow(landmark: landmarks[5])
-        LandmarkRow(landmark: landmarks[6])
-        LandmarkRow(landmark: landmarks[7])
-        LandmarkRow(landmark: landmarks[8])
-        LandmarkRow(landmark: landmarks[9])
-        LandmarkRow(landmark: landmarks[10])
-        LandmarkRow(landmark: landmarks[11])
+        List(landmarks, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
     }
 }
 
